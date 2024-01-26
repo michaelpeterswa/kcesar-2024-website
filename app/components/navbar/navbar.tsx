4e473dd0d8db6d22204d35a6f7755fec87d37097
Michael Peters <michaelpeterswa@icloud.com>
2024-01-25 20:27:33 -0800

    fix: update a bunch of things

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -3,16 +3,55 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="navbar sticky top-0 z-50 text-white">
-      {/* TODO: add scroll transition to navbar at some point */}
-      <div className="flex-1">
+    <div className="navbar sticky top-0 z-50">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
         <div
           tabIndex={0}
           role="button"
-          className="btn btn-ghost btn-circle avatar"
+          className="btn btn-ghost btn-circle avatar mx-2"
         >
           <Link href="/">
-            <div className="w-10 rounded-full">
+            <div className="w-12 rounded-full">
               <Image
                 alt="Tailwind CSS Navbar component"
                 src="https://kcesar.org/assets/logo/logo_kcesar_300x300.png"
@@ -23,8 +62,8 @@ export default function Navbar() {
           </Link>
         </div>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-3">
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
           <li className="px-1">
             <Link href="/join-us">Join Us</Link>
           </li>
@@ -50,12 +89,23 @@ export default function Navbar() {
               </ul>
             </details>
           </li>
-          <li className="px-1">
-            <Link href="/donate" className="bg-orange-500 text-white">
-              Donate
-            </Link>
-          </li>
         </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="menu menu-horizontal px-1 gap-2">
+          <Link
+            href="/donate"
+            className="btn btn-ghost bg-orange-500 text-white"
+          >
+            Donate
+          </Link>
+          <Link
+            href="/emergency"
+            className="btn btn-ghost bg-red-500 text-white"
+          >
+            Emergency
+          </Link>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Button from "@mui/material/Button";
 import React from "react";
-import Navbar from "./components/navbar/navbar";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,7 +10,7 @@ export default function Home() {
         loop
         muted
         playsInline
-        className="fixed -z-10 w-screen h-screen object-cover top-0 left-0"
+        className="fixed -z-20 w-screen h-screen object-cover top-0 left-0 brightness-75"
       >
         <source
           src="https://mptrswa.com/pendorielle1080.mp4"
@@ -19,10 +18,19 @@ export default function Home() {
         />
       </video>
       <div className="h-screen flex text-center items-center justify-center">
-        <div className="max-w-6xl p-10 mt-20 backdrop-brightness-75 rounded-3xl backdrop-blur-sm">
-          <h1 className="text-7xl neuzeit-light text-slate-50">
+        {/* <div className="max-w-6xl p-10 backdrop-brightness-75 rounded-3xl backdrop-blur-sm"> */}
+        <div className="max-w-6xl p-10">
+          <Image
+            alt="Tailwind CSS Navbar component"
+            src="https://kcesar.org/assets/logo/logo_kcesar_300x300.png"
+            width={200}
+            height={200}
+            className="drop-shadow inline-block pb-10"
+          />
+          <h1 className="text-7xl neuzeit-light text-slate-50 drop-shadow">
             King County Explorer Search and Rescue
           </h1>
+          <br />
           <p className="py-6 text-slate-100">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
