4269fb2cbbb5ccc078c0261c222128db27d1c2ae
Michael Peters <michaelpeterswa@icloud.com>
2024-01-26 09:29:22 -0800

    fix: adding new pages and making more responsive/clean

---
+++ app/emergency/page.tsx
@@ -0,0 +1,7 @@
+export default async function Emergency() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl">Emergency!!! ⛑️</h1>
+    </div>
+  );
+}
