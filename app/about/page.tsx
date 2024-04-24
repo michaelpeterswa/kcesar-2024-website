b73a57aafeda324d0e2699f191ed3d4889639ac2
Michael Peters <michael@michaelpeterswa.com>
2024-04-23 19:30:30 -0700

    fix: lint
    Signed-off-by: Michael Peters <michael@michaelpeterswa.com>

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -45,13 +45,13 @@ export default async function About() {
       <p className="text-xl pt-8 text-justify px-20 container">
         King County Explorer Search & Rescue was founded in 1954 as one of the
         first organized search and rescue teams in the country, and the first to
-        accept youth members. The "Explorer" in Explorer Search & Rescue comes
-        from ESAR's affiliation with the Boy Scouts of America Learning for Life
-        program. King County ESAR still maintains a Post with the Boy Scouts of
-        America but we are not as directly associated with Scouting as we once
-        were. Today, we are the largest of eight member-units of the King County
-        Search & Rescue Association (KCSARA) and accept both adult and youth
-        members each training season.
+        accept youth members. The &quot;Explorer&quot; in Explorer Search &
+        Rescue comes from ESAR&apos;s affiliation with the Boy Scouts of America
+        Learning for Life program. King County ESAR still maintains a Post with
+        the Boy Scouts of America but we are not as directly associated with
+        Scouting as we once were. Today, we are the largest of eight
+        member-units of the King County Search & Rescue Association (KCSARA) and
+        accept both adult and youth members each training season.
       </p>
       <Link href="/about/history" className="btn bg-esar-green text-white mt-8">
         Our History
