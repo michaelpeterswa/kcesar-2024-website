08f56921a365c8697d256199ffb7c31567ebb122
Michael Peters Jr <michael@michaelpeterswa.com>
2024-08-23 23:03:17 -0700

    fix: add width constraint to the basicimagetall

--- components/image/basicimage.tsx
+++ components/image/basicimage.tsx
@@ -27,7 +27,7 @@ export function BasicImageTall({
   alt: string;
 }) {
   return (
-    <div className="relative h-96 w-3/4">
+    <div className="relative h-96 w-3/4 max-w-5xl">
       <Image
         alt={alt}
         src={location}
